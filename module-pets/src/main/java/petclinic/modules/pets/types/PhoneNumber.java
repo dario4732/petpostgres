71c5e4329840c47d68a201c3ec5fd3f2bf78b665
Dan Haywood <dan@haywood-associates.co.uk>
2021-10-02 16:55:14 +0100

    updates PhoneNumber with regex pattern.

--- module-pets/src/main/java/petclinic/modules/pets/types/PhoneNumber.java
+++ module-pets/src/main/java/petclinic/modules/pets/types/PhoneNumber.java
@@ -15,7 +15,11 @@ import org.apache.isis.applib.annotation.PropertyLayout;
 @Property(
         editing = Editing.ENABLED,
         maxLength = PhoneNumber.MAX_LEN,
-        optionality = Optionality.OPTIONAL
+        optionality = Optionality.OPTIONAL,
+        regexPattern = "[+]?[0-9 ]+",
+        regexPatternReplacement =
+                "Specify only numbers and spaces, optionally prefixed with '+'.  " +
+                        "For example, '+353 1 555 1234', or '07123 456789'"
 )
 @Parameter(maxLength = PhoneNumber.MAX_LEN, optionality = Optionality.OPTIONAL)
 @Target({ ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.ANNOTATION_TYPE })
