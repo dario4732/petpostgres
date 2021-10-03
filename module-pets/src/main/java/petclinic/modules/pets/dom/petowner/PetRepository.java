f3b04319c7db0389750a5836c8fe88da5c90f61f
Dan Haywood <dan@haywood-associates.co.uk>
2021-10-03 16:23:08 +0100

    adds findByPetOwnerAndName to PetRepository

--- module-pets/src/main/java/petclinic/modules/pets/dom/petowner/PetRepository.java
+++ module-pets/src/main/java/petclinic/modules/pets/dom/petowner/PetRepository.java
@@ -1,10 +1,12 @@
 package petclinic.modules.pets.dom.petowner;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.repository.Repository;
 
 public interface PetRepository extends Repository<Pet, Long> {
 
     List<Pet> findByPetOwner(PetOwner petOwner);
+    Optional<Pet> findByPetOwnerAndName(PetOwner petOwner, String name);
 }
