c1cda008861dd3bfaf405b0fad819955e933d09f
Dan Haywood <dan@haywood-associates.co.uk>
2021-10-05 14:46:22 +0100

    updates webapp to depend upon visits module
    ... which depends in turn on pets module

--- webapp/src/main/java/petclinic/webapp/application/ApplicationModule.java
+++ webapp/src/main/java/petclinic/webapp/application/ApplicationModule.java
@@ -4,10 +4,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
-import petclinic.modules.pets.PetsModule;
+import petclinic.modules.visits.VisitsModule;
 
 @Configuration
-@Import(PetsModule.class)
+@Import(VisitsModule.class)
 @ComponentScan
 public class ApplicationModule {
 
