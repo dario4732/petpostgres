05e843759a9859cf4f40fce1b6becd841f5280d7
Dan Haywood <dan@haywood-associates.co.uk>
2021-10-05 19:04:18 +0100

    updates VisitRepository to inherit from JpaRepository rather than
    ... just Repository

--- module-visits/src/main/java/petclinic/modules/visits/dom/visit/VisitRepository.java
+++ module-visits/src/main/java/petclinic/modules/visits/dom/visit/VisitRepository.java
@@ -1,14 +1,12 @@
 package petclinic.modules.visits.dom.visit;
 
 import java.util.List;
-import java.util.Optional;
 
-import org.springframework.data.repository.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import petclinic.modules.pets.dom.pet.Pet;
-import petclinic.modules.pets.dom.petowner.PetOwner;
 
-public interface VisitRepository extends Repository<Visit, Long> {
+public interface VisitRepository extends JpaRepository<Visit, Long> {
 
     List<Visit> findByPetOrderByVisitAtDesc(Pet pet);
 
