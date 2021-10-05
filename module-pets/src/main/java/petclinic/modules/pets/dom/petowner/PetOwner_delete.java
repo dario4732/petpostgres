80928551f258229e598fedcf9f193a05da20981d
Dan Haywood <dan@haywood-associates.co.uk>
2021-10-05 18:30:49 +0100

    reworks PetOwner_delete to emit domain events.

--- module-pets/src/main/java/petclinic/modules/pets/dom/petowner/PetOwner_delete.java
+++ module-pets/src/main/java/petclinic/modules/pets/dom/petowner/PetOwner_delete.java
@@ -6,14 +6,13 @@ import org.apache.isis.applib.annotation.Action;
 import org.apache.isis.applib.annotation.ActionLayout;
 import org.apache.isis.applib.annotation.Publishing;
 import org.apache.isis.applib.annotation.SemanticsOf;
+import org.apache.isis.applib.events.domain.ActionDomainEvent;
 import org.apache.isis.applib.services.repository.RepositoryService;
-import org.apache.isis.applib.services.xactn.TransactionService;
 
 import lombok.RequiredArgsConstructor;
 
-import petclinic.modules.pets.dom.pet.PetRepository;
-
 @Action(
+        domainEvent = PetOwner_delete.ActionEvent.class,
         semantics = SemanticsOf.NON_IDEMPOTENT_ARE_YOU_SURE,
         commandPublishing = Publishing.ENABLED,
         executionPublishing = Publishing.ENABLED
@@ -24,17 +23,14 @@ import petclinic.modules.pets.dom.pet.PetRepository;
 @RequiredArgsConstructor
 public class PetOwner_delete {
 
+    public static class ActionEvent extends ActionDomainEvent<PetOwner_delete>{}
+
     private final PetOwner petOwner;
 
-    public void act(
-            ) {
-        petRepository.findByPetOwner(petOwner).forEach(repositoryService::remove);
-        transactionService.flushTransaction();
+    public void act() {
         repositoryService.remove(petOwner);
         return;
     }
 
-    @Inject PetRepository petRepository;
     @Inject RepositoryService repositoryService;
-    @Inject TransactionService transactionService;
 }
